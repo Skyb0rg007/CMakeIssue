4352bb8296d76cd89e2b3123e793e6408225fb83
Skye Soss <skyler.soss@gmail.com>
2018-12-15 19:06:59 -0500

    Added test run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+(
+    mkdir proj1/build
+    cd proj1/build || exit
+    cmake ..
+    cmake --build . --target install
+)
+
+(
+    mkdir proj2/build
+    cd proj2/build || exit
+    echo "###### Running cmake #######"
+    cmake ..
+    echo "###### Running cmake again #######"
+    cmake ..
+)
+
+rm -r proj1_install/ proj1/build proj2/build
